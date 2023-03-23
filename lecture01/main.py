38a3281718f111e83265c7e6cd07c250befab7d9
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2023-03-23 23:36:14 +0300

    First Practice in Python

---
+++ lecture01/main.py
@@ -0,0 +1,9 @@
+print("Hello, user!")
+
+# Input
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+# Output
+print("Username", username, sep=": ")
+print("Password", password, sep=": ")
