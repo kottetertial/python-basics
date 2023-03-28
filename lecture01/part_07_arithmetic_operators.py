95106f4b7aacaa451926e75851686e718ea1f1b8
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2023-03-28 22:00:41 +0300

    Arithmetic Operators in Python: Part I

---
+++ lecture01/part_07_arithmetic_operators.py
@@ -0,0 +1,30 @@
+# Addition, subtraction
+print(11 + 2)
+print(11 - 2)
+
+# Multiplication, exponentiation
+print(11 * 2)
+print(11 ** 2)
+
+# Division, integer division, modulo division
+print(11 / 2)
+print(11 // 2)
+print(11 % 2)
+
+# When will you wake up if you go to bed at 23:00 and sleep for 7 hrs?
+print((23 + 7) % 24)
+
+# When do you have to go to bed if you need to sleep for 7 hrs and wake up at 06:00?
+print((6 - 7) % 24)
+
+# Strings
+print(2 * "2")
+print("2" + "2")
+
+# Use variables
+part1 = int(input())
+part2 = int(input())
+part3 = int(input())
+part4 = int(input())
+
+print("Your grade:", 0.25 * part1 + 0.25 * part2 + 0.25 * part3 + 0.25 * part4)
