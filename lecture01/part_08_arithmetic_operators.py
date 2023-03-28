8cc5dbeafe69763f06929e9f3f25d2f43dd8eb56
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2023-03-28 22:09:42 +0300

    Arithmetic Operators in Python: Part II

---
+++ lecture01/part_08_arithmetic_operators.py
@@ -0,0 +1,13 @@
+# String multiplication
+n = int(input())
+print("A" * n)
+
+# String concatenation
+name = input("What is your name? ")
+print("Hello", name + "!")
+
+expression = "2 + 2 = "
+result = 2 + 2
+print(expression + str(result))
+print(type(result))
+print(result * n)
