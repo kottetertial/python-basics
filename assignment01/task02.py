6dad5ce6aa563afa57738e1f846e57b37cffd9bd
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2023-03-29 20:18:24 +0300

    Programming assignments - 1

---
+++ assignment01/task02.py
@@ -0,0 +1,4 @@
+n = int(input())
+print("/\___/\    " * n)
+print("(=^o^=)    " * n)
+print("(\") (\")__/ " * n)
---
+++ assignment01/task07.py
@@ -0,0 +1,2 @@
+c = float(input())
+print(c, "°C is equal to ", c * 9 / 5 + 32, "°F", sep="")
