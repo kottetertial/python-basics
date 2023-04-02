a4b87282de331bf7ebaa7600f55fca1391ca8a9e
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2023-04-02 21:32:48 +0300

    Math Module Practice

---
+++ lecture01/part_10_math_module_practice.py
@@ -0,0 +1,15 @@
+import math
+from math import sqrt
+from math import e as exp
+
+print(math.sqrt(16))
+print(sqrt(16))
+
+print(math.pi)
+print(exp)
+
+print(math.log(exp))
+print(math.log10(1000))
+
+print(math.sin(0))
+print(math.cos(0))
