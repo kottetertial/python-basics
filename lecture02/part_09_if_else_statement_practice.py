14b7adc601399916308c32556d011ccbc32f9ef3
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2023-04-06 00:09:36 +0300

    If-Else Statement Practice

---
+++ lecture02/part_09_if_else_statement_practice.py
@@ -0,0 +1,11 @@
+num = int(input())
+if num > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative or zero.")
+
+traffic_fraction = float(input())
+if traffic_fraction >= 0.5:
+    print("The traffic is heavy.")
+else:
+    print("The traffic is low.")
